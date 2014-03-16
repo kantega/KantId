a05bdf1247e46112c4d1d4b869ff7e2910e3b681
qtips <ahm.qadeer@gmail.com>
2014-03-17 00:18:01 +0100

    Finished birthdate and old validity check. Note that new Danish numbers given after 2007 do not necessarily pass the validity check.

--- kantid/src/main/java/no/kantega/id/dk/DanishIdNumber.java
+++ kantid/src/main/java/no/kantega/id/dk/DanishIdNumber.java
@@ -9,6 +9,10 @@ import java.util.Locale;
 
 public class DanishIdNumber extends LocalIdNumber {
 
+    /**
+     * Official documentation
+     * https://cpr.dk/media/167692/personnummeret%20i%20cpr.pdf
+     */
     private static final Locale LOCALE_DENMARK = new Locale("da", "DK");
 
     public DanishIdNumber(String idToken, Locale locale) {
@@ -20,7 +24,7 @@ public class DanishIdNumber extends LocalIdNumber {
     }
 
     public boolean isValid() {
-        return super.isValid(DanishIdNumber::valid);
+        return super.isValid(DanishIdNumber::validate1968);
     }
 
     public Gender gender() {
@@ -28,15 +32,66 @@ public class DanishIdNumber extends LocalIdNumber {
     }
 
     public LocalDate birthday() {
-        return null;
+        return super.birthday(DanishIdNumber::determineBirthdate);
+
     }
 
-    public static boolean valid(final IdNumber number) {
-        return false;
+    public static boolean validate1968(final IdNumber number) {
+        if (!number.getIdToken().matches("(\\d){10}")) {
+            return false;
+        }
+
+        int controlSum =
+            4 * Character.getNumericValue(number.getIdToken().charAt(0)) +
+            3 * Character.getNumericValue(number.getIdToken().charAt(1)) +
+            2 * Character.getNumericValue(number.getIdToken().charAt(2)) +
+            7 * Character.getNumericValue(number.getIdToken().charAt(3)) +
+            6 * Character.getNumericValue(number.getIdToken().charAt(4)) +
+            5 * Character.getNumericValue(number.getIdToken().charAt(5)) +
+            4 * Character.getNumericValue(number.getIdToken().charAt(6)) +
+            3 * Character.getNumericValue(number.getIdToken().charAt(7)) +
+            2 * Character.getNumericValue(number.getIdToken().charAt(8)) +
+            Character.getNumericValue(number.getIdToken().charAt(9));
+        return controlSum % 11 == 0;
     }
 
     public static Gender determineGender(final IdNumber number) {
         return (Character.getNumericValue(number.getIdToken().charAt(9)) & 1) == 0 ? Gender.FEMALE : Gender.MALE;
     }
 
+    public static LocalDate determineBirthdate(final IdNumber number) {
+        if (!validate1968(number)) {
+            return null; // should throw exception
+        }
+
+        int day=Integer.parseInt(number.getIdToken().substring(0,2));
+        int month=Integer.parseInt(number.getIdToken().substring(2,4));
+        int shortYear=Integer.parseInt(number.getIdToken().substring(4,6));
+
+        int year = shortYear + findYear(shortYear, Character.getNumericValue(number.getIdToken().charAt(7)));
+
+        return LocalDate.of(year, month, day);
+    }
+
+    private static int findYear(int shortYear, int yearCenturyPart) {
+        if (yearCenturyPart>=0 && yearCenturyPart <=3) {
+            return 1900;
+        }
+        else if (yearCenturyPart==4||yearCenturyPart==9)   {
+            if (shortYear >=0 || shortYear <=36) {
+                return 2000;
+            }
+            else {
+                return 1900;
+            }
+        }
+        else {
+            if (shortYear>=58 && shortYear<=99) {
+                return 1800;
+            }
+            else {
+                return 2000;
+            }
+        }
+    }
 }
--- kantid/src/test/java/no/kantega/id/dk/DanishIdNumberTest.java
+++ kantid/src/test/java/no/kantega/id/dk/DanishIdNumberTest.java
@@ -14,10 +14,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class DanishIdNumberTest {
 
     private static final String MALE_ID1 = "0204850011";
+    private static final String MALE_ID2 = "0504156907";
     private static final String FEMALE_ID1 = "0204850372";
     @Test
     public void testValidity() {
         assertThat(forId(MALE_ID1).isValid(),is(true));
+        assertThat(forId(FEMALE_ID1).isValid(),is(true));
+        assertThat(forId("1234567890").isValid(),is(false));
+        assertThat(forId("020485-0011").isValid(),is(false));
     }
 
     @Test
@@ -28,7 +32,8 @@ public class DanishIdNumberTest {
 
     @Test
     public void testBirthDate() {
-        assertThat(forId(MALE_ID1).birthday(), is(LocalDate.of(1985, Month.DECEMBER, 2)));
+        assertThat(forId(MALE_ID1).birthday(), is(LocalDate.of(1985, Month.APRIL, 2)));
+        assertThat(forId(MALE_ID2).birthday(), is(LocalDate.of(2015, Month.APRIL, 5)));
     }
 
 }
