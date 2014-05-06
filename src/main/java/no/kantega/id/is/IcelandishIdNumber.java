9e8a13d906b7d3498fea7913bf489db61cf76368
Kristoffer Skaret <kristoffer.skaret@kantega.no>
2014-05-06 14:30:00 +0200

    birthday function

--- src/main/java/no/kantega/id/is/IcelandishIdNumber.java
+++ src/main/java/no/kantega/id/is/IcelandishIdNumber.java
@@ -1,8 +1,14 @@
 package no.kantega.id.is;
 
+import no.kantega.id.api.IdNumber;
 import no.kantega.id.api.LocalIdNumber;
 
+import java.time.LocalDate;
 import java.util.Locale;
+import java.util.Optional;
+
+import static java.lang.Character.getNumericValue;
+import static java.lang.Integer.parseInt;
 
 /**
  * Created by kristofferskaret on 14.04.14.
@@ -27,4 +33,39 @@ public class IcelandishIdNumber extends LocalIdNumber {
     protected boolean supports(Locale locale) {
         return LOCALE_ICELAND.getCountry().equals(locale.getCountry());
     }
+
+    public static boolean valid(final IdNumber idNumber) {
+        return true;
+    }
+
+    /**
+     * Calculates optional birthday for the given id number. Calculation is based on
+     * 7 first characters of the idNumber where the 7th character represents century.
+     *
+     * @param idNumber where birthday is calculated from.
+     * @return optional birthday, or empty in case of invalid date or invalid idNumber format.
+     */
+    public static Optional<LocalDate> birthday(final IdNumber idNumber) {
+
+        int day = parseInt(idNumber.getIdToken().substring(0, 2));
+        int month = parseInt(idNumber.getIdToken().substring(2, 4));
+        int shortYear = parseInt(idNumber.getIdToken().substring(4, 6));
+
+        int centuryDigit = getNumericValue(idNumber.getIdToken().charAt(10));
+        int year = calculateYear(shortYear, centuryDigit);
+
+        return Optional.of(LocalDate.of(year, month, day));
+    }
+
+    private static int calculateYear(int shortYear, int centuryDigit) {
+
+        int century;
+        if (centuryDigit == 0) {
+            century = 2000;
+        } else {
+            century = 1000 + 100 * centuryDigit;
+        }
+        return century + shortYear;
+    }
+
 }
--- src/test/java/no/kantega/id/is/IcelandishIdNumberTest.java
+++ src/test/java/no/kantega/id/is/IcelandishIdNumberTest.java
@@ -1,7 +1,12 @@
 package no.kantega.id.is;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
+import static java.util.Locale.FRANCE;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -9,19 +14,44 @@ import static org.junit.Assert.assertTrue;
  */
 public class IcelandishIdNumberTest {
 
+    private static final String VALID_ICE_IDNUMBER = "120174-3389";
+
     @Test
     public void icelandishIdNumberSupportsLocale() throws Exception {
-        assertTrue(IcelandishIdNumber.forId("312321").supports(IcelandishIdNumber.LOCALE_ICELAND));
+        assertTrue(IcelandishIdNumber.forId(VALID_ICE_IDNUMBER).supports(IcelandishIdNumber.LOCALE_ICELAND));
     }
 
     @Test
-    public void icelandishIdNumberSupportsLocaleGiven() throws Exception {
-        assertTrue(IcelandishIdNumber.forId("312321", IcelandishIdNumber.LOCALE_ICELAND).supports(IcelandishIdNumber.LOCALE_ICELAND));
+    public void icelandishIdNumberSupportsValidLocale() throws Exception {
+        assertTrue(IcelandishIdNumber.forId(VALID_ICE_IDNUMBER, IcelandishIdNumber.LOCALE_ICELAND).supports(IcelandishIdNumber.LOCALE_ICELAND));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void emptyValueIsNotSupported() {
         IcelandishIdNumber.forId(null);
     }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidLocaleIsIllegal() {
+        IcelandishIdNumber.forId(VALID_ICE_IDNUMBER, FRANCE);
+    }
+
+    @Test
+    public void validIdNumberValidates() {
+        assertTrue(IcelandishIdNumber.forId(VALID_ICE_IDNUMBER).isValid(IcelandishIdNumber::valid));
+    }
+
+    @Test
+    public void mustReturnCorrectBirtday() {
+        assertThat(IcelandishIdNumber.forId(VALID_ICE_IDNUMBER).birthday(IcelandishIdNumber::birthday).get(), CoreMatchers.is(LocalDate.of(1974, 01, 12)));
+    }
+
+    @Test
+    public void mustReturnCorrectAge() {
+
+    }
+
+
 }
     
